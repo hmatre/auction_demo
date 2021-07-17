355e6dc0341933fb9fcc5a943c936c6bf425d8b4
Hitesh <hiteshmatre@gmail.com>
2021-07-17 19:12:06 +0530

    fixes for jquery pagination

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,20 +8,3 @@
 
 
 //= require bootstrap
-
-$(document).ready(function(){
-  $('#sort_by').change(function(){
-    sort_by =  $(this)
-    $.ajax({
-      url: "/?sort_by="+$(this).val(),
-    }).done(function(data) {
-      sort_by
-      // sort_by.parent().parent().find('.coupon_list').parent().find('select').html('')
-      // sort_by.parent().parent().find('.coupon_list').parent().find('select').append('<option></option>')
-      // $.each(data, function(){
-      //   sort_by.parent().parent().find('.coupon_list').parent().find('select').append('<option value="'+ (this.id) +'">'+ this.name +'</option>')
-      // })
-    });
-  })
-})
-
