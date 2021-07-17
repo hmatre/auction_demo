acbe6db5312db35be50c881d0a3fd7b28d9e6ac3
Hitesh <hiteshmatre@gmail.com>
2021-07-17 22:47:04 +0530

    add js template

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,18 @@ class HomeController < ApplicationController
 
   def index
     if params[:sort_by] == "category_name"
-      @projects = Project.joins(:user, :category).order("categories.name ASC").limit(2)
+      @projects = Project.joins(:user, :category).order("categories.name ASC")
     elsif params[:sort_by] == "user_name"
-      @projects = Project.joins(:user, :category).order("users.user_name ASC").limit(2)
+      @projects = Project.joins(:user, :category).order("users.user_name ASC")
     elsif params[:sort_by] == "project_title"
-      @projects = Project.joins(:user, :category).order("title ASC").limit(2)
+      @projects = Project.joins(:user, :category).order("title ASC")
     else
-      @projects = Project.joins(:user, :category).order('created_at DESC').limit(6)
+      @projects = Project.joins(:user, :category).order('created_at DESC')
+    end
+
+    respond_to do |format|
+      format.js {render 'index.js.erb', layout: false}
+      format.html  # new.html.erb
     end
   end
 end
